c6c155d449e3611da37c5cb8a1fb2b422a10a9c1
“Stephanie <stephanie.techarmour@gmail.com>
2018-07-11 21:09:53 -0400

    pseudocoded the JS logic

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-let startButton = document.querySelector('#startbutton')
 // when you click the startButton it starts the game
+// COMPUTER DOES:
+/* Start game means, on click the circles I need write a code that:
+     1) Have the buttons flash a random sequence
+       --find out how to make a random sequence and have something flash in JS */
+let startButton = document.querySelector('#startbutton')
 
-// on click of these buttons below, add animation flash..or have it disappear
+// USER DOES: after the random sequence flashes, then player needs to input the same sequence. 
+/* This means I need to write code that checks a logic with an if statement. If the user input === computer's sequence then generates a new sequence & increase score*/
+
+/*This also means I need to code on click for the buttons to recognize user input so something like on click light up. If user input === computer sequence then just new sequence, but no score raise. */
+
 let redCircle = document.querySelector('.test1')
 let yellowCircle = document.querySelector('.test2')
 let greenCircle = document.querySelector('.test3')
 let blueCircle = document.querySelector('.test3')
 
 // on click of this button, reset the score
-let scoreButton = document.querySelector('#reset')
+let resetButton = document.querySelector('.reset')
