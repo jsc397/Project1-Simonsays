02401b0b374a7cbe7878eb4e4fa66a6dade85ce8
“Stephanie <stephanie.techarmour@gmail.com>
2018-07-12 15:33:50 -0400

    broke my js but got awesome gif background

--- main.js
+++ main.js
@@ -1,40 +1,44 @@
-// when you click the startButton it starts the game
-// COMPUTER DOES:
-/* Start game means, on click the circles I need write a code that:
-     1) Have the buttons flash a random sequence
-       --find out how to make a random sequence and have something flash in JS */
-let startButton = document.querySelector('#startbutton')
 
-// USER DOES: after the random sequence flashes, then player needs to input the same sequence. 
-/* This means I need to write code that checks a logic with an if statement. If the user input === computer's sequence then generates a new sequence & increase score*/
-
-/*This also means I need to code on click for the buttons to recognize user input so something like on click light up. If user input === computer sequence then just new sequence, but no score raise. */
-
+//  Variables
+var startButton = document.querySelector('#startbutton')
 let redCircle = document.querySelector('.test1')
 let yellowCircle = document.querySelector('.test2')
 let greenCircle = document.querySelector('.test3')
 let blueCircle = document.querySelector('.test3')
-
-// on click of this button, reset the score
 let resetButton = document.querySelector('.reset')
 
-// ask about transform-translate to shake the color buttons
+// click events
+// startButton.addEventListener('click', startGame)
+// on click, you need start game, which is generate random array, but also light up the buttons to show user
+redCircle.addEventListener('click', function () {alert('red Button clicked')})
+yellowCircle.addEventListener('click', function () {alert('yellow Button clicked')})
+greenCircle.addEventListener('click', function () { alert('green Button clicked')})
+blueCircle.addEventListener('click', function () { alert('blue Button clicked')})
+resetButton.addEventListener('click', function () { alert('reset Button clicked') })
 
-/* two options.... I can have a color array... that */
-// console.log(possibleColorArray)
+// functions
+// To start game, push Start button to generate random sequence and light up circles
+// var answerArray = [];
+// let possibleColorArray = ["red", "yellow", "green", "blue"];
+
+// function startGame ()
+// {   for (i = 0; i < possibleColorArray.length; i++) {
+//     answerArray.push(possibleColorArray[Math.floor(Math.random() * possibleColorArray.length)];
+//     conole.log(answerArray)
+//     )}
+// }
+// startButton.addEventListener('click', startGame)
 
 
-let answerArray = [];
-let possibleColorArray = ["red", "yellow", "green", "blue"];
-for (i = 0; i < possibleColorArray.length; i++) {
-    // var rand = myArray[Math.floor(Math.random() * myArray.length)];
-    answerArray.push(possibleColorArray[Math.floor(Math.random() * possibleColorArray.length)]
+/*
+// when you click the startButton it starts the game
+// COMPUTER DOES:
+/* Start game means, on click the circles I need write a code that:
+     1) Have the buttons flash a random sequence
+       -- and have something flash in JS */
 
-    //    answerArray += possibleColorArray(Math.floor(Math.random() * possibleColorArray.length))
-        //return answerArray;
-    )
-};
-// put the answers for the random array into answer array
-// then have another variable called userInput and store the user's clicked array into userInput array.
+// Then have another variable called userInput and store the user's clicked array into userInput array.
 // then check if answer array is equal to userInputArray.
 // If not,.....we'lll think about that one later
+
+// on click of this button, reset the score
