e96ae6eaca14b5f73d904083cf1a49bd2d7a8abb
“Stephanie <stephanie.techarmour@gmail.com>
2018-07-11 20:48:11 -0400

    added blueCircle variable and scoreButton variable

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 let startButton = document.querySelector('#startbutton')
+// when you click the startButton it starts the game
+
+// on click of these buttons below, add animation flash..or have it disappear
+let redCircle = document.querySelector('.test1')
+let yellowCircle = document.querySelector('.test2')
+let greenCircle = document.querySelector('.test3')
+let blueCircle = document.querySelector('.test3')
+
+// on click of this button, reset the score
+let scoreButton = document.querySelector('#reset')
