cc1bf073f2e2a16c6c8adbb381d5dc770b0f5703
“Stephanie <stephanie.techarmour@gmail.com>
2018-07-12 19:01:08 -0400

    successfully changed background and circle colors. names unchanged and js unchanged yet

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 
 //  Variables
 var startButton = document.querySelector('#startbutton')
-let redCircle = document.querySelector('.test1')
-let yellowCircle = document.querySelector('.test2')
-let greenCircle = document.querySelector('.test3')
-let blueCircle = document.querySelector('.test3')
-let resetButton = document.querySelector('.reset')
+// let redCircle = document.querySelector('.test1')
+let yellowCircle = document.querySelector('.yellow')
+let greenCircle = document.querySelector('.green')
+let blueCircle = document.querySelector('.blue')
+let resetButton = document.querySelector('.reset') 
 
 // click events
 // startButton.addEventListener('click', startGame)
 // on click, you need start game, which is generate random array, but also light up the buttons to show user
-redCircle.addEventListener('click', buttonClick)
+// redCircle.addEventListener('click', buttonClick)
 yellowCircle.addEventListener('click', buttonClick)
 greenCircle.addEventListener('click', buttonClick)
 blueCircle.addEventListener('click', buttonClick)
@@ -26,13 +26,24 @@ function startGame () {
     answerArray.push(possibleColorArray[Math.floor(Math.random() * possibleColorArray.length)]
     )}
 }
-
-// function for buttonClick
-var userClickArry [];
-function buttonClick () {
-
-
+// checking the red 
+let redCircle = document.querySelector('.test1')
+redCircle.addEventListener('click', buttonClick)
+function checkUser () {
+    if userClick[0] === answerArray[0] {
+        // move on
+    }
 }
+
+
+
+
+// // function for buttonClick
+// var userClickArry [];
+// function buttonClick () {
+
+
+// }
 /*
 // when you click the startButton it starts the game
 // COMPUTER DOES:
