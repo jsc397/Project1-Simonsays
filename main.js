65358a393ad0f21971a3f869f1769ebf1a47bb6c
“Stephanie <stephanie.techarmour@gmail.com>
2018-07-12 22:49:36 -0400

    correct colors in js and css and fixed the js so that the start button works again

--- main.js
+++ main.js
@@ -1,47 +1,43 @@
 
 //  Variables
 var startButton = document.querySelector('#startbutton')
-// let redCircle = document.querySelector('.test1')
-let yellowCircle = document.querySelector('.yellow')
-let greenCircle = document.querySelector('.green')
-let blueCircle = document.querySelector('.blue')
+// let grayCircle = document.querySelector('.gray')
+let seaGreenCircle = document.querySelector('.sgreen')
+let darkBlueCircle = document.querySelector('.dblue')
+let whiteCircle = document.querySelector('.white')
 let resetButton = document.querySelector('.reset') 
 
 // click events
 // startButton.addEventListener('click', startGame)
 // on click, you need start game, which is generate random array, but also light up the buttons to show user
-// redCircle.addEventListener('click', buttonClick)
-yellowCircle.addEventListener('click', buttonClick)
-greenCircle.addEventListener('click', buttonClick)
-blueCircle.addEventListener('click', buttonClick)
-resetButton.addEventListener('click', function () { alert('reset Button clicked') })
+// grayCircle.addEventListener('click', buttonClick)
+// seaGreenCircle.addEventListener('click', buttonClick)
+darkBlueCircle.addEventListener('click', function () {alert('reset Button clicked') })
+// whiteCircle.addEventListener('click', buttonClick)
+resetButton.addEventListener('click', function () {alert('reset Button clicked') })
 
 // functions
 // To start game, push Start button to generate random sequence and light up circles
 var answerArray = [];
-let possibleColorArray = ["red", "yellow", "green", "blue"];
+let possibleColorArray = ["gray", "sgreen", "dblue", "white"];
 
 function startGame () {
-     for (i = 0; i < possibleColorArray.length; i++) {
+  for (i = 0; i < possibleColorArray.length; i++) {
     answerArray.push(possibleColorArray[Math.floor(Math.random() * possibleColorArray.length)]
-    )}
+)}
 }
-// checking the red 
-let redCircle = document.querySelector('.test1')
-redCircle.addEventListener('click', buttonClick)
-function checkUser () {
-    if userClick[0] === answerArray[0] {
-        // move on
-    }
-}
-
-
-
+// // checking the gray
+// let grayCircle = document.querySelector('.test1')
+// grayCircle.addEventListener('click', buttonClick)
+// function checkUser () {
+//     if userClick[0] === answerArray[0] {
+//         // move on
+//     }
+// }
 
 // // function for buttonClick
 // var userClickArry [];
 // function buttonClick () {
-
 
 // }
 /*
