aa5b0c771832b13effa320fb67932a7d497792c5
“Stephanie <stephanie.techarmour@gmail.com>
2018-07-15 17:10:48 -0400

    fixed html and css but broke js. the circles will not register clicks anymore.

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
 
 //  Variables
-var startButton = document.querySelector('#startbutton')
-   
-let resetButton = document.querySelector('.reset') 
+// let startButton = document.querySelector('#startbutton')
+// let resetButton = document.querySelector('.reset') 
 
-/*
-Click Event test-notes
-resetButton.addEventListener('click', function () {location.reload('Try Again!') })
-location.reload(forceGet)
-*/
 
 // functions
-// To start game, push Start button to generate random sequence and light up circles
-
+// To start game, push Start button to generate random sequence
+let startButton = document.querySelector('#startbutton')
 let possibleColorArray = ["gray", "sgreen", "dblue", "white"];
 
 function startGame () {
@@ -24,26 +18,29 @@ console.log(answerArray)
 }
 
 //  capturing user clicks
- NodeList.prototype.addEventListener = function (class_name, callback) {
-    for (var i = 0; i < this.length; i++) {
-        this[i].addEventListener(class_name, callback);
-    }
-};
+let circles = document.querySelectorAll('.circle')
+let cicleArray = Array.from(circles).forEach(addListener)
+// function addListener() {
 
-document.querySelectorAll(".circle").addEventListener("click", function () {
-    alert('clicked');
-});
+// }
 
-// TEST THE TWO ARRAYS
-//      if (/* user click */ === 
+// function eventListener {
 
-/* when you click the startButton it starts the game
-    COMPUTER DOES:
-    Start game means, on click the circles I need write a code that:
-     1) Have the buttons flash a random sequence
-       -- and have something flash in JS
-     2) Then have another variable called userInput and store the user's    clicked array into userInput array.
-       then check if answer array is equal to userInputArray.
-     3) If not, spit another random array
-     4) click of this button, reset the score
-     */
+// }
+
+// Array.from(NodeList);
+// console.log(circles)
+
+// let myArray = Array.from(nl)
+//  NodeList.prototype.addEventListener = function (class_name, callback) {
+//     for (var i = 0; i < this.length; i++) {
+//         this[i].addEventListener(class_name, callback);
+//     }
+// };
+
+// document.querySelectorAll("circle").addEventListener("click", function () {
+//     alert('clicked');
+// });
+
+
+
